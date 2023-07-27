78d847e497810af46080206d6daef40dc0ee3395
JonasSeifried <69268825+JonasSeifried@users.noreply.github.com>
2023-07-27 11:50:38 +0200

    renameRegion command + colored region messages

--- src/main/java/tondoa/regions/commands/RegionCommand.java
+++ src/main/java/tondoa/regions/commands/RegionCommand.java
@@ -7,7 +7,9 @@ import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Style;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
@@ -26,10 +28,36 @@ public class RegionCommand {
                 .then(literal("del")
                     .then(argument("region", StringArgumentType.word())
                     .executes(ctx -> delRegion(ctx.getSource(), StringArgumentType.getString(ctx, "region")))))
+                .then(literal("rename")
+                    .then(argument("region", StringArgumentType.word())
+                        .then(argument("newName", StringArgumentType.word())
+                            .executes(ctx -> renameRegion(ctx.getSource(), StringArgumentType.getString(ctx, "region"), StringArgumentType.getString(ctx, "newName"))))))
                 .then(argument("region", StringArgumentType.word())
                         .executes(ctx -> addRegion(ctx.getSource(), StringArgumentType.getString(ctx,"region"))))
                 .executes(ctx -> addRegion(ctx.getSource(), null)));
 
+    }
+
+    private static int renameRegion(ServerCommandSource source, String region, String newName) throws CommandSyntaxException {
+        ServerPlayerEntity player = source.getPlayer();
+        if (player == null) {
+            throw new SimpleCommandExceptionType(Text.translatable("tondoas-region.command.region.not_player")).create();
+        }
+        PlayerState playerState = ServerState.getPlayerState(player);
+        if (!playerState.regions.containsKey(region)) {
+            throw new SimpleCommandExceptionType(Text.translatable("tondoas-region.command.region.not_found", region)).create();
+        }
+        if (playerState.regions.containsKey(newName)) {
+            throw new SimpleCommandExceptionType(Text.translatable("tondoas-region.command.region.already_used", newName)).create();
+        }
+
+        TRegion nTRegion = new TRegion(playerState.regions.get(region), newName);
+        playerState.regions.put(newName, nTRegion);
+        playerState.regions.remove(region);
+        ServerState.markDirty(player.server);
+        player.sendMessage(Text.translatable("tondoas-region.command.region.renamed", region, newName)
+                .setStyle(Style.EMPTY.withColor(Formatting.GREEN)));
+        return 1;
     }
 
     public static int delRegion(ServerCommandSource source, String region) throws CommandSyntaxException {
@@ -40,28 +68,31 @@ public class RegionCommand {
         PlayerState playerState = ServerState.getPlayerState(player);
 
         if (!playerState.regions.containsKey(region)) {
-            player.sendMessage(Text.translatable("tondoas-region.command.region.delete.not_found", region));
+            player.sendMessage(Text.translatable("tondoas-region.command.region.not_found", region)
+                    .setStyle(Style.EMPTY.withColor(Formatting.YELLOW)));
             return 0;
         }
 
         playerState.regions.remove(region);
         ServerState.markDirty(player.server);
-        player.sendMessage(Text.translatable("tondoas-region.command.region.delete.deleted"));
+        player.sendMessage(Text.translatable("tondoas-region.command.region.delete.deleted")
+                .setStyle(Style.EMPTY.withColor(Formatting.RED)));
         return 1;
 
     }
 
     public static int addRegion(ServerCommandSource source, String region) {
-        if (source.getPlayer() == null) return -1;
+        ServerPlayerEntity player = source.getPlayer();
+        if (player == null) return -1;
 
         Vec3d coords = source.getPosition();
-        RegistryEntry<Biome> biomeRegistry = source.getPlayer().getWorld().getBiome(BlockPos.ofFloored(coords));
+        RegistryEntry<Biome> biomeRegistry = player.getWorld().getBiome(BlockPos.ofFloored(coords));
         if (biomeRegistry.getKey().isEmpty())
             return -1;
         Identifier biomeIdentifier = biomeRegistry.getKey().get().getValue();
 
 
-        PlayerState playerState = ServerState.getPlayerState(source.getPlayer());
+        PlayerState playerState = ServerState.getPlayerState(player);
 
         TRegion tRegion;
         if (region == null)
@@ -70,13 +101,14 @@ public class RegionCommand {
             tRegion = new TRegion(coords, biomeIdentifier, region);
 
         if (playerState.regions.containsKey(tRegion.name)) {
-            source.getPlayer().sendMessage(Text.translatable(tRegion.name).append(" is already being used!"));
+            player.sendMessage(Text.translatable(
+                    "tondoas-region.command.region.already_used", tRegion.name));
             return 0;
         }
         playerState.regions.put(tRegion.name, tRegion);
-        ServerState.getServerState(source.getPlayer().server).markDirty();
+        ServerState.markDirty(player.server);
 
-        source.getPlayer().sendMessage(tRegion.getText());
+        player.sendMessage(tRegion.getText());
         return 1;
     }
 }
