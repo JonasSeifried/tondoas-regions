7c536d8240f60b3f58543b68ce03820dd6ec329b
JonasSeifried <69268825+JonasSeifried@users.noreply.github.com>
2023-07-27 11:08:09 +0200

    fixed new line bug

--- src/main/java/tondoa/regions/commands/RegionsCommand.java
+++ src/main/java/tondoa/regions/commands/RegionsCommand.java
@@ -3,7 +3,9 @@ package tondoa.regions.commands;
 import com.mojang.brigadier.CommandDispatcher;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.MutableText;
+import net.minecraft.text.Style;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import tondoa.regions.persistent_data.PlayerState;
 import tondoa.regions.persistent_data.ServerState;
 import tondoa.regions.persistent_data.TRegion;
@@ -22,10 +24,11 @@ public class RegionsCommand {
         if (source.getPlayer() == null) return -1;
         PlayerState playerState = ServerState.getPlayerState(source.getPlayer());
         MutableText text = Text.literal("List of all your regions\n");
+        int c = playerState.regions.size();
         for( TRegion region : playerState.regions.values()) {
-            text.append(region.getText()).append("\n");
+            text.append(region.getText());
+            if (c-- != 1) text.append("\n");
         }
-
         source.getPlayer().sendMessage(text);
         return 1;
     }
