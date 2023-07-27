bdd9d98ff86cac970aeb389e9a43600f464383c5
JonasSeifried <69268825+JonasSeifried@users.noreply.github.com>
2023-07-27 11:09:03 +0200

    new markDirty function

--- src/main/java/tondoa/regions/persistent_data/ServerState.java
+++ src/main/java/tondoa/regions/persistent_data/ServerState.java
@@ -83,4 +83,8 @@ public class ServerState extends PersistentState {
 
         return serverState.players.computeIfAbsent(player.getUuid(), uuid -> new PlayerState());
     }
+
+    public static void markDirty(MinecraftServer server) {
+        getServerState(server).markDirty();
+    }
 }
