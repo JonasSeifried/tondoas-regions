319cba8d0d452c1e1288b10ad532a6d0ea30210d
JonasSeifried <69268825+JonasSeifried@users.noreply.github.com>
2023-07-29 13:58:43 +0200

    simple gui prototype

---
+++ src/main/java/tondoa/regions/gui/RegionScreen.java
@@ -0,0 +1,10 @@
+package tondoa.regions.gui;
+
+import io.github.cottonmc.cotton.gui.GuiDescription;
+import io.github.cottonmc.cotton.gui.client.CottonClientScreen;
+
+public class RegionScreen extends CottonClientScreen {
+    public RegionScreen(GuiDescription description) {
+        super(description);
+    }
+}
