ffecd05900fe25d9f77812f69aa911dc1da5dd81
JonasSeifried <69268825+JonasSeifried@users.noreply.github.com>
2023-08-06 23:46:45 +0200

    removed commands /region del, /region rename and /regions

--- src/main/java/tondoa/regions/commands/CommandInitializer.java
+++ src/main/java/tondoa/regions/commands/CommandInitializer.java
@@ -7,7 +7,6 @@ public class CommandInitializer {
 
     public static void registerCommands() {
         ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> RegionCommand.register(dispatcher));
-        ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> RegionsCommand.register(dispatcher));
         RegionMod.LOGGER.info("Registering Mod Commands for " + RegionMod.MOD_ID);
 
     }
--- src/main/java/tondoa/regions/commands/RegionCommand.java
+++ src/main/java/tondoa/regions/commands/RegionCommand.java
@@ -24,55 +24,9 @@ import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.lit
 public class RegionCommand {
     public static void register(CommandDispatcher<FabricClientCommandSource> dispatcher) {
         dispatcher.register(literal("region")
-                .then(literal("del")
-                    .then(argument("region", StringArgumentType.word())
-                    .executes(ctx -> delRegion(ctx.getSource(), StringArgumentType.getString(ctx, "region")))))
-                .then(literal("rename")
-                    .then(argument("region", StringArgumentType.word())
-                        .then(argument("newName", StringArgumentType.word())
-                            .executes(ctx -> renameRegion(ctx.getSource(), StringArgumentType.getString(ctx, "region"), StringArgumentType.getString(ctx, "newName"))))))
                 .then(argument("region", StringArgumentType.word())
                         .executes(ctx -> addRegion(ctx.getSource(), StringArgumentType.getString(ctx,"region"))))
                 .executes(ctx -> addRegion(ctx.getSource(), null)));
-
-    }
-    private static int renameRegion(FabricClientCommandSource source, String region, String newName) throws CommandSyntaxException {
-        ClientPlayerEntity player = source.getPlayer();
-        if (player == null) {
-            throw new SimpleCommandExceptionType(Text.translatable("tondoas-regions.command.region.not_player")).create();
-        }
-
-        if (!DataStorage.regions.containsKey(region)) {
-            throw new SimpleCommandExceptionType(Text.translatable("tondoas-regions.command.region.not_found", region)).create();
-        }
-        if (DataStorage.regions.containsKey(newName)) {
-            throw new SimpleCommandExceptionType(Text.translatable("tondoas-regions.name_already_used", newName)).create();
-        }
-
-        TRegion nTRegion = new TRegion(DataStorage.regions.get(region), newName);
-        DataStorage.regions.put(newName, nTRegion);
-        DataStorage.regions.remove(region);
-        player.sendMessage(Text.translatable("tondoas-regions.command.region.renamed", region, newName)
-                .setStyle(Style.EMPTY.withColor(Formatting.GREEN)));
-        return 1;
-    }
-
-    public static int delRegion(FabricClientCommandSource source, String region) throws CommandSyntaxException {
-        ClientPlayerEntity player = source.getPlayer();
-        if (player == null) {
-            throw new SimpleCommandExceptionType(Text.translatable("tondoas-regions.command.region.not_player")).create();
-        }
-
-        if (DataStorage.regions.containsKey(region)) {
-            player.sendMessage(Text.translatable("tondoas-regions.command.region.not_found", region)
-                    .setStyle(Style.EMPTY.withColor(Formatting.YELLOW)));
-            return 0;
-        }
-
-        DataStorage.regions.remove(region);
-        player.sendMessage(Text.translatable("tondoas-regions.command.region.delete.deleted")
-                .setStyle(Style.EMPTY.withColor(Formatting.RED)));
-        return 1;
 
     }
 
