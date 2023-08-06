4a31dfeed2dfa29dc821a0e73131d765bc08be73
JonasSeifried <69268825+JonasSeifried@users.noreply.github.com>
2023-08-06 23:33:44 +0200

    fixed add region keybinding

--- src/main/java/tondoa/regions/gui/RegionScreen.java
+++ src/main/java/tondoa/regions/gui/RegionScreen.java
@@ -11,6 +11,12 @@ import tondoa.regions.gui.components.RegionListViewComponent;
 
 public class RegionScreen extends BaseOwoScreen<FlowLayout> {
 
+    private boolean openAddRegionModal = false;
+    public RegionScreen() {
+    }
+    public RegionScreen(boolean openAddRegionModal) {
+        this.openAddRegionModal = openAddRegionModal;
+    }
     @Override
     protected @NotNull OwoUIAdapter<FlowLayout> createAdapter() {
         return OwoUIAdapter.create(this, Containers::verticalFlow);
@@ -22,9 +28,14 @@ public class RegionScreen extends BaseOwoScreen<FlowLayout> {
                 .surface(Surface.VANILLA_TRANSLUCENT)
                 .horizontalAlignment(HorizontalAlignment.CENTER);
 
+        RegionListViewComponent regionListViewComponent = new RegionListViewComponent();
+        if (openAddRegionModal) {
+            regionListViewComponent.createAddRegionModal();
+        }
+
         rootComponent
                 .child(Components.label(Text.literal("Tondoas Regions")).margins(Insets.of(5,20,0,0)))
-                .child(new RegionListViewComponent());
+                .child(regionListViewComponent);
 
     }
 }
--- src/main/java/tondoa/regions/keyBindings/RegionKeyBinding.java
+++ src/main/java/tondoa/regions/keyBindings/RegionKeyBinding.java
@@ -21,6 +21,7 @@ public class RegionKeyBinding {
                 client.setScreen(new RegionScreen());
             }
             while (createRegionKey.wasPressed()) {
+                client.setScreen(new RegionScreen(true));
             }
         });
 
