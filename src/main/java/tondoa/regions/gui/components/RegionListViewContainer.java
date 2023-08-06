e1bb3bff28480da9d4383a69522b9be81c2d88fc
JonasSeifried <69268825+JonasSeifried@users.noreply.github.com>
2023-08-06 16:13:58 +0200

    delete button

--- src/main/java/tondoa/regions/gui/components/RegionListViewContainer.java
+++ src/main/java/tondoa/regions/gui/components/RegionListViewContainer.java
@@ -21,11 +21,12 @@ public final class RegionListViewContainer {
         FlowLayout parentContainer = Containers.verticalFlow(Sizing.fill(60), Sizing.content());
         parentContainer.horizontalAlignment(HorizontalAlignment.CENTER);
         TextBoxComponent textBox = Components.textBox(Sizing.fill(80));
+        textBox.setPlaceholder(Text.translatable("tondoas-regions.gui.search_placeholder"));
         parentContainer.child(textBox);
         parentContainer.gap(5);
 
 
-        FlowLayout container =  Containers.verticalFlow(Sizing.content(), Sizing.content());
+        FlowLayout container = Containers.verticalFlow(Sizing.content(), Sizing.content());
         DataStorage.sortedRegions().forEach(t -> container.child(buildChild(t)));
         textBox.onChanged().subscribe(s -> {
             container.clearChildren();
@@ -34,33 +35,48 @@ public final class RegionListViewContainer {
                     .forEach(t -> container.child(buildChild(t)));
         });
 
-        parentContainer.child(Containers.horizontalScroll(Sizing.fill(100), Sizing.fill(50),
-                Containers.verticalScroll(Sizing.content(), Sizing.fill(96), container)));
+        parentContainer.child(Containers.verticalScroll(Sizing.content(), Sizing.fill(50), container));
 
         return parentContainer
                 .surface(Surface.VANILLA_TRANSLUCENT)
-                .padding(Insets.of(5,2,5,5));
+                .padding(Insets.of(5, 2, 5, 5));
     }
 
-    private static Component buildChild(TRegion tRegion) {
-        return Containers.horizontalFlow(Sizing.content(), Sizing.content())
-                        .child(
-                                Components.label(Text.literal(tRegion.name))
-                                        .horizontalSizing(Sizing.fill(33))
-                        )
-                        .child(
-                                Components.label(Text.literal(String.format("%.0f %.0f %.0f",
-                                                tRegion.x, tRegion.y, tRegion.z)))
-                                        .horizontalTextAlignment(HorizontalAlignment.CENTER)
-                                        .horizontalSizing(Sizing.fill(33))
-                        )
-                        .child(
-                                Components.label(
-                                        tRegion.getTranslatedBiome())
-                                        .color(Color.ofRgb(0x808080))
-                                        .horizontalTextAlignment(HorizontalAlignment.RIGHT)
-                                        .horizontalSizing(Sizing.content())
-                        );
+    private static FlowLayout buildChild(TRegion tRegion) {
+        FlowLayout container = Containers.horizontalFlow(Sizing.fill(100), Sizing.content());
+
+        container
+                .child(Components.button(
+                        Text.literal("X"),
+                        button -> {
+                            DataStorage.regions.remove(tRegion.name);
+                            assert button.parent() != null;
+                            assert button.parent().parent() != null;
+                            button.parent().parent().removeChild(button.parent());
+                        })
+                        .horizontalSizing(Sizing.fill(5)))
+                .child(Components.button(
+                        Text.literal(".."),
+                        button -> {
+
+                        })
+                        .horizontalSizing(Sizing.fill(5))
+                        .margins(Insets.right(5)))
+                .child(Components.label(Text.literal(tRegion.name))
+                        .verticalTextAlignment(VerticalAlignment.CENTER)
+                        .horizontalSizing(Sizing.fill(30)))
+                .child(Components.label(Text.literal(String.format("%.0f %.0f %.0f", tRegion.x, tRegion.y, tRegion.z)))
+                        .horizontalSizing(Sizing.fill(30)))
+                .child(Components.label(tRegion.getTranslatedBiome()).color(Color.ofRgb(0x808080))
+                        .horizontalSizing(Sizing.fill(30)));
+
+        container
+                .verticalAlignment(VerticalAlignment.CENTER)
+                .surface(Surface.DARK_PANEL)
+                .padding(Insets.of(4))
+                .margins(Insets.bottom(2));
+
+        return container;
     }
 
 }
